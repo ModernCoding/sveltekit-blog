6248b3c7855d86d01e6492453302d6690a392d42
Cormorant Programming <cormorant.programming@gmail.com>
2022-02-26 17:15:40 +0700

    layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,22 @@
+<div class="container">
+  <slot />
+</div>
+
+
+<style type="text/css">
+
+  @import '../../static/css/layout.css';
+
+  :global(*) {
+    box-sizing: border-box;
+  }
+
+  :global(body) {
+    margin: 0;
+  }
+
+  :global(p) {
+    text-align: justify;
+  }
+  
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script type="text/javascript" context="module">
+
+  export const load = ({ fetch }) => {
+
+    return {
+      
+      props: {
+        message: "ok"
+      }
+
+    }
+
+  }
+
+</script>
+
+
+<script type="text/javascript">
+  export let message
+</script>
+
+
+<h1>Posts - {message}</h1>
+
+<p>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc et tortor at mauris bibendum sagittis. Phasellus pulvinar quam tempor laoreet luctus. Pellentesque non sodales tortor. Nullam non turpis purus. Duis est tortor, vestibulum eget turpis id, euismod semper metus. Integer egestas vehicula massa ut porttitor. Vivamus in purus felis. Nulla condimentum dui vitae dolor viverra, vel hendrerit lacus ultrices. Quisque volutpat elit erat, et laoreet velit porttitor at. Sed efficitur sapien eu ipsum dignissim semper. Vestibulum pharetra, dui quis mattis ullamcorper, quam lectus hendrerit ligula, a vestibulum urna nisi eu nulla. Morbi non imperdiet nibh. Etiam dignissim vel nulla eget sagittis. Fusce et erat non orci faucibus mattis. Duis gravida consequat orci, non efficitur neque dictum nec. Pellentesque dui nulla, consequat quis mauris mattis, congue tempus sem.
+</p>
